e5ce6c83f1e5baf222975593bad5997e1f8dd47f
syontyagi <syontyagi1109@gmail.com>
2023-04-23 17:26:53 -0700

    updated comparemain.c

--- comparemain.c
+++ comparemain.c
@@ -4,13 +4,13 @@ int main(int argc, char *argv[])
 {
 	int a[11] = {7, 6, 4, 3, 8, 9, 1, 2, 10, 11};
 	int searchterm = 3;
-	int i=3, ret;
+	int i=5, ret;
 	ret = compare(a, i, searchterm);
 	if(ret == -1) {
-		printf("Not Equal");
+		printf("\nNot Equal\n");
 	}
 	else {
-		printf("matches at index %d", i);
+		printf("\nMatches at index %d\n", i);
 	}
 	return 0;
 }
