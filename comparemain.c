126e2516ea4dd2b7356af4210da97a1e314a0925
syontyagi <syontyagi1109@gmail.com>
2023-04-23 17:20:26 -0700

    updated compare main

--- comparemain.c
+++ comparemain.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
-#include "swap.h"
 #include "compare.h"
 
 int main(int argc, char *argv[])
 {
 	int a[11] = {7, 6, 4, 3, 8, 9, 1, 2, 10, 11};
-	int i=0, j=5, ret;
-	ret = compare(a, i, j);
-	printf("%d, %d, %d, %d, %d\n", i, j, a[i], a[j], ret);
-	swap(a, i, j);
-	printf("%d, %d, %d, %d, %d\n", i, j, a[i], a[j], ret);
+	int searchterm = 3;
+	int i=3, ret;
+	ret = compare(a, i, searchterm);
+	if(ret == -1) {
+		printf("Not Equal");
+	}
+	else {
+		printf("matches at index %d", i);
+	}
 	return 0;
 }
