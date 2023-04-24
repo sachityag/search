8ce603f481c3077e208be779ffb544457ef956cf
sachityag <sachityagi0807@gmail.com>
2023-04-23 17:10:58 -0700

    fixed searchmain and makefile

--- searchmain.c
+++ searchmain.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "bubblesort.h"
-#include "swap.h"
 #include "compare.h"
+#include "search.h"
 
 
 
@@ -11,6 +10,8 @@ int main(int argc, char *argv[])
 {
 	int i;
 	int len;
+	int searchterm = 5;
+	int retval;
 	int a[20] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 	if(argc > 21) {
 		printf("Your array must be less than 21!!\n");
@@ -20,6 +21,10 @@ int main(int argc, char *argv[])
 	for(i=0;i<len;i++) {
 		a[i] = atoi(argv[i+1]);
 	}
-	bubblesort(a, len);
+	retval = search(a, len);
+	if(retval == -1) {
+		printf("Search not found\n");
+	}
+
 	return 0;
 }
